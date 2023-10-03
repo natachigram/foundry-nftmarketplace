41a84a9a04c90dbccad8503b89e57113e1aa6aa0
MACBOOK <natachigram@gmail.com>
2023-10-03 16:32:24 +0100

    solmate

--- src/Counter.sol
+++ src/Counter.sol
@@ -3,6 +3,8 @@ pragma solidity ^0.8.13;
 
 contract Counter {
     uint256 public number;
+    uint128 public a;
+    uint128 public b;
 
     function setNumber(uint256 newNumber) public {
         number = newNumber;
@@ -10,5 +12,10 @@ contract Counter {
 
     function increment() public {
         number++;
+    }
+
+    function packValues(uint128 _a, uint128 _b) public {
+        a = _a;
+        b = _b;
     }
 }
--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -10,6 +10,7 @@ contract CounterTest is Test {
     function setUp() public {
         counter = new Counter();
         counter.setNumber(0);
+        counter.packValues(12, 14);
     }
 
     function test_Increment() public {
@@ -20,5 +21,25 @@ contract CounterTest is Test {
     function testFuzz_SetNumber(uint256 x) public {
         counter.setNumber(x);
         assertEq(counter.number(), x);
+    }
+
+    function testSetSlotValues() public {
+        counter.packValues(12, 14);
+    }
+
+    function testSlotValues() public {
+        bytes32 res = vm.load(address(counter), bytes32(uint256(1)));
+        // assertEq(
+        //     abi.encodePacked(counter.a() + counter.b()),
+        //     abi.encodePacked(counter.a(), res_a)
+        // );
+        uint128 a;
+        uint256 b;
+        a = uint128(uint256(res));
+        b = uint128(uint256(res) >> 128);
+        console2.logUint(a);
+        console2.logUint(b);
+        assertEq(counter.b(), b);
+        assertEq(counter.a(), a);
     }
 }
