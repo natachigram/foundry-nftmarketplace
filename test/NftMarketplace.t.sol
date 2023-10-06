50cbce6cd21f3c821b9bb0f79c803911d87316f5
MACBOOK <natachigram@gmail.com>
2023-10-06 06:34:23 +0100

    rewriting test

--- test/NftMarketplace.t.sol
+++ test/NftMarketplace.t.sol
@@ -3,63 +3,45 @@ pragma solidity ^0.8.13;
 
 import {Test} from "lib/forge-std/src/Test.sol";
 import {NFTMarketplace} from "../src/NftMarketplace.sol";
-import "lib/openzeppelin-contracts/contracts/token/ERC721/IERC721.sol";
-import "lib/openzeppelin-contracts/contracts/token/ERC721/extensions/IERC721Enumerable.sol";
-import "lib/openzeppelin-contracts/contracts/access/Ownable.sol";
+import {Nft} from "../src/Nft.sol";
+import "./HelperFunc.sol";
 
 contract CounterTest is Test {
     NFTMarketplace public nftMarketPlace;
+    address owner;
+    address addr1;
+    address addr2;
+
+    uint256 privKeyA;
+    uint256 privKeyB;
+
+    NFTMarketplace.order o;
 
     function setUp() public {
         nftMarketPlace = new NFTMarketplace();
+        nft = new NFT();
 
-        address seller;
-        address tokenAddress = 0x25D0e89E6Df7ae8C0E8D1D9Bd0991CbE17d10628;
-        uint256 tokenId = 1;
-        uint256 price = 5;
-        uint256 deadline = 466252191;
+        (userA, privKeyA) = mkaddr("USERA");
+        (userB, privKeyB) = mkaddr("USERB");
 
-        seller = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
-        bytes32 digest = keccak256(
-            abi.encodePacked(seller, tokenAddress, tokenId, price, deadline)
-        );
-        (uint8 v, bytes32 r, bytes32 s) = vm.sign(
-            0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80,
-            digest
-        );
-        bytes memory signature = abi.encodePacked(r, s, v);
-        nftMarketPlace.createOrder(
-            tokenAddress,
-            tokenId,
-            price,
-            signature,
-            deadline
-        );
+        o = NFTMarketplace.Order({
+            seller: address(0),
+            tokenAddress: address(nft),
+            tokenId: 1,
+            price: 2 ether,
+            active: false,
+            signature: bytes(""),
+            deadline: 0
+        });
+
+        nft.mint(addr1, 1);
     }
 
-    function testCreatOrder() public {
-        address tokenAddress = 0x25D0e89E6Df7ae8C0E8D1D9Bd0991CbE17d10628;
-        uint256 tokenId = 1;
-        uint256 price = 5;
-        uint256 deadline = 5 minutes;
-        address seller = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
-        bytes32 digest = keccak256(
-            abi.encodePacked(seller, tokenAddress, tokenId, price, deadline)
-        );
-        (uint8 v, bytes32 r, bytes32 s) = vm.sign(
-            0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80,
-            digest
-        );
-        bytes memory signature = abi.encodePacked(r, s, v);
+    function testOwnerCannotCreateOrder() public {
+        o.seller = addr2;
+        switchSigner(addr2);
 
-        vm.expectRevert(bytes("Only token owner can create a listing"));
-
-        NFTMarketplace.createOrder(
-            tokenAddress,
-            tokenId,
-            price,
-            signature,
-            deadline
-        );
+        vm.expectRevert("Only token owner can create a listing");
+        nftMarketPlace.createOrder(o);
     }
 }
